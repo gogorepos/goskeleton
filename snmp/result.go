217e89b0793d04aee8782d33c580c6c08c51c159
yleoer <yleoer@gmail.com>
2021-05-24 10:11:53 +0800

    fix(snmp): modified result.go

--- snmp/result.go
+++ snmp/result.go
@@ -2,35 +2,54 @@ package snmp
 
 import (
 	"strconv"
+	"strings"
 
 	"github.com/gosnmp/gosnmp"
 )
 
 type Result struct {
-	Data gosnmp.SnmpPDU
+	gosnmp.SnmpPDU
 }
 
 func (r Result) String() string {
-	switch r.Data.Type {
-	case gosnmp.Integer:
-		return strconv.Itoa(r.Data.Value.(int))
+	if n, ok := r.Value.([]byte); ok {
+		return string(n)
 	}
-	return string(r.Data.Value.([]byte))
+	return ""
 }
 
 func (r Result) Int() int {
-	return r.Data.Value.(int)
+	if n, ok := r.Value.(int); ok {
+		return n
+	}
+	return 0
 }
 
 func (r Result) Uint() uint {
-	return r.Data.Value.(uint)
+	if n, ok := r.Value.(uint); ok {
+		return n
+	}
+	return 0
 }
 
-func (r Result) UintSlice() []uint {
-	s := r.Data.Value.([]uint8)
-	result := make([]uint, 0, len(s))
-	for _, u := range s {
-		result = append(result, uint(u))
+func (r Result) MacString() string {
+	builder := strings.Builder{}
+	if v, ok := r.Value.([]uint8); ok {
+		// 不是一个规范的 mac 地址，返回空字符串
+		if len(v) != 6 {
+			return ""
+		}
+		for _, u := range v {
+			// 将十进制数字转为大写形式的十六进制的字符串
+			n := strings.ToUpper(strconv.FormatUint(uint64(u), 16))
+			// 如果字符串只有一位，补零
+			if len(n) < 2 {
+				builder.WriteString("0" + n + ":")
+			} else {
+				builder.WriteString(n + ":")
+			}
+		}
 	}
-	return result
+	// 去除最后一个 ":" 冒号
+	return builder.String()[:builder.Len()-1]
 }
